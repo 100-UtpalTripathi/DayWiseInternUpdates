e64546b1b0b22ee90f73c75842eed511df2eee13
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-11 16:34:10 +0530

    Application 2 completed!

---
+++ Day3/Day3LabTaskSolutions/Day3LabTask/App2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3LabTask
+{
+    internal class App2
+    {
+        public void myMethod()
+        {
+            int greatest = int.MinValue;
+            string? num;
+            int n;
+            Console.WriteLine("Enter numbers (enter a negative number to stop):");
+            
+            do
+            {
+                Console.Write("Type a number, and then press Enter: ");
+                num = Console.ReadLine();
+
+                int cleanNum1;
+                while (!int.TryParse(num, out cleanNum1))
+                {
+                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                    num = Console.ReadLine();
+                }
+
+                n = Convert.ToInt32(num);
+                if (n >= 0 && n > greatest)
+                {
+                    greatest = n;
+                }
+            } while (n >= 0);
+
+            Console.WriteLine("The greatest number entered is: " + greatest);
+        }
+    }
+}
