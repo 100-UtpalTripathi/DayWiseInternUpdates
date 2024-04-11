be40f95551222f8e83424b16040f4b810c99f2c8
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-11 16:56:43 +0530

    Application 5 implementation completed!

--- Day3/Day3LabTaskSolutions/Day3LabTask/App5.cs
+++ Day3/Day3LabTaskSolutions/Day3LabTask/App5.cs
@@ -8,9 +8,50 @@ namespace Day3LabTask
 {
     internal class App5
     {
+        static string takeInput(string printVal)
+        {
+            string? temp;
+            do
+            {
+                temp = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(temp))
+                    Console.Write($"\nInvalid Credentials. Enter {printVal} again: ");
+
+            } while (string.IsNullOrWhiteSpace(temp));
+
+            return temp;
+        }
         public void myMethod()
         {
-            Console.WriteLine("Hii");
+            const string validUsername = "ABC";
+            const string validPassword = "123";
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.Write("Enter username: ");
+                string username = takeInput("username");
+
+                Console.Write("Enter password: ");
+                string password = takeInput("password");
+
+                if (username == validUsername && password == validPassword)
+                {
+                    Console.WriteLine("Login successful!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid credentials! Please try again.");
+                    attempts++;
+                }
+            }
+
+            if (attempts == 3)
+            {
+                Console.WriteLine("You have exceeded the maximum number of attempts. Access denied.");
+            }
         }
     }
 }
