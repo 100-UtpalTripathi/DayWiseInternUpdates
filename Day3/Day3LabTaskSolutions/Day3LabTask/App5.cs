384ec1a61a705b099cefe72c807a58e28b2d7ee0
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-11 16:46:30 +0530

    Created classes for app5 and app6

---
+++ Day3/Day3LabTaskSolutions/Day3LabTask/App5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3LabTask
+{
+    internal class App5
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Hii");
+        }
+    }
+}
