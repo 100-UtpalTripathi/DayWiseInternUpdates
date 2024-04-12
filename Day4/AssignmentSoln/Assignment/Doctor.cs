230e44f27d3b12cc3552f82caee8a8daa2226139
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-12 15:36:42 +0530

    Assignmet: Doctor class created with properties

---
+++ Day4/AssignmentSoln/Assignment/Doctor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Doctor
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public int Exp { get; private set; }
+        public string Qualification { get; private set; }
+        public string Speciality { get; private set; }
+
+        public Doctor()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Age = 0;
+            Exp = 0;
+            Qualification = string.Empty;
+            Speciality = string.Empty;
+        }
+        public Doctor(int id, string name, int age, int exp, string qualification, string speciality)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Exp = exp;
+            Qualification = qualification;
+            Speciality = speciality;
+        }
+        static void PrintDoctors(Doctor[] doctors)
+        {
+            foreach (var doctor in doctors)
+            {
+                Console.WriteLine($"Id: {doctor.Id}, Name: {doctor.Name}, Age: {doctor.Age}, " +
+                    $"Exp: {doctor.Exp}, Qualification: {doctor.Qualification}, " +
+                    $"Speciality: {doctor.Speciality}");
+            }
+        }
+
+    }
+}
---
+++ Day4/AssignmentSoln/Assignment/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Xml.Linq;
+
+namespace Assignment
+{
+    
+    internal class Program
+    {
+        string takingStringInput()
+        {
+            string? str;
+            do
+            {
+                str = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(str))
+                    Console.Write("\nInvalid input, Please Enter again:\n");
+
+            } while (string.IsNullOrWhiteSpace(str));
+            return str;
+
+        }
+        int takingIntInput()
+        {
+            int? id;
+
+        }
+        Doctor createDoctors()
+        {
+            int id;
+            string? name;
+            Console.Write("Id: ");
+
+            while (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.WriteLine("Invalid input! Please enter a valid integer for Id:");
+            }
+
+
+            do
+            {
+                Console.Write("\nName: ");
+                name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name))
+                    Console.WriteLine("Invalid input. Please enter a valid name!\n");
+
+            } while (string.IsNullOrWhiteSpace(name));
+
+            int age;
+            do
+            {
+                Console.Write("Age: ");
+            } while (!int.TryParse(Console.ReadLine(), out age) || age <= 0);
+
+            int exp;
+            do
+            {
+                Console.Write("Experience (in years): ");
+            } while (!int.TryParse(Console.ReadLine(), out exp) || exp < 0);
+
+
+
+            Doctor d = new Doctor(id, name, age, exp, qualification, speciality);
+            return d;
+        }
+        static void Main(string[] args)
+        {
+            Doctor[] doctors = new Doctor[3];
+            
+            for(int i = 0; i < doctors.Length; i++)
+            {
+                Console.WriteLine($"Enter details for Doctor {i + 1}:");
+
+            }
+        }
+    }
+}
