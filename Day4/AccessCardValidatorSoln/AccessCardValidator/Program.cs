af6afe6227771853705f010a5a8ab8aa8846ab87
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-12 16:38:08 +0530

    Access Card Validator Application Setup!

---
+++ Day4/AccessCardValidatorSoln/AccessCardValidator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AccessCardValidator
+{
+
+    internal class Program
+    {
+        static string TakingStringInput(string PrintValue)
+        {
+            string? str;
+            do
+            {
+                str = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(str))
+                    Console.Write($"Invalid input, Please Enter {PrintValue} again:\n");
+
+            } while (string.IsNullOrWhiteSpace(str));
+            return str;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Card Validator Application!\n");
+            Console.Write("Enter the Card Number:");
+            string CardNumber = TakingStringInput("Card Number");
+        }
+    }
+}
