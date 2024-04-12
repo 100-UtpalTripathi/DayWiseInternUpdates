517a11881f2f4c8ec58f90221ef85b78ba108a29
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-12 11:21:37 +0530

    Added another class Employee, getters, setters etc.

---
+++ Day4/classesObjectsSoln/classesObjects/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesObjects
+{
+    class Employee
+    {
+        double salary;
+
+        public string Name { get; set; }
+
+        public double Salary
+        {
+            set
+            {
+                salary = value;
+            }
+            get
+            {
+                return (salary - (salary * 10 / 100));
+            }
+        }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public void Work()
+        {
+            Console.WriteLine("Works");
+        }
+    }
+}
---
+++ Day4/classesObjectsSoln/classesObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace classesObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Ramu";
+            employee.Salary = 10000;
+            employee.DateOfBirth = new DateTime(2000, 12, 18);
+            employee.Email = "ramu@abccorp.com";
+            Employee employee2 = new Employee
+            {
+                Name = "Somu",
+                DateOfBirth = new DateTime(2000, 3, 6),
+                Email = "somu@abccorp.com",
+                Salary = 40000
+            };
+
+            Console.WriteLine(employee2.Salary);
+            employee.Work();
+        }
+    }
+}
