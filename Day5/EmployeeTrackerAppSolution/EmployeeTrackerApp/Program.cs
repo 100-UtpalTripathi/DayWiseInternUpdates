dedff80f5535aff04ac1e2598343316e446a3f4f
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-15 10:09:56 +0530

    Sequence & selection in C# implementation!

---
+++ Day5/EmployeeTrackerAppSolution/EmployeeTrackerApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EmployeeTrackerApp
+{
+    internal class Program
+    {
+        void UnderstandingSequenceStatements()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hi");
+            int num1, num2;
+            num1 = 100;
+            num2 = 20;
+            int num3 = num1 / num2;
+            Console.WriteLine($"The result of {num1} divided by {num2} is {num3}");
+        }
+        void UnderstandingSelectionWithIf()
+        {
+            string strName = "Albatross";
+            if (strName == "Albatross")
+            {
+                Console.WriteLine("Welcome Albatross. you are authorized! ");
+                Console.WriteLine("Bingo!!");
+            }
+            else if (strName == "Somu")
+                Console.WriteLine("You are Somu not Albatross. ONly Basic access");
+            else
+                Console.WriteLine("I don't know who you are. Get out...");
+
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.UnderstandingSequenceStatements();
+            program.UnderstandingSelectionWithIf();
+
+        }
+    }
+}
