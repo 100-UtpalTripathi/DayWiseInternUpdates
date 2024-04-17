27724b5bc2945697745b83d65937654c17b2658b
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-17 11:03:19 +0530

    Understood and implemented HashSet!

--- Day7/RequestTrackerApplication/RequestTrackerApplication/Program.cs
+++ Day7/RequestTrackerApplication/RequestTrackerApplication/Program.cs
@@ -5,25 +5,32 @@ namespace RequestTrackerApp
 {
     internal class Program
     {
-        void UnderstandingList()
+        void UnderstandingSet()
         {
-            List<int> list = new List<int>();
-            list.Add(0);
-            list.Add(1);
-            list.Add(2);    
-                
-            list.Add(3);
-            list.Add(4);
-            list.Add(5);
+            HashSet<int> mySet = new HashSet<int>();
 
-            foreach(int num in list)
+            // Adding elements to the set
+            mySet.Add(5);
+            mySet.Add(10);
+            mySet.Add(15);
+
+            // Adding a duplicate element (it will be ignored)
+            mySet.Add(10);
+
+            // Checking if an element exists in the set
+            Console.WriteLine("Is 5 in the set? " + mySet.Contains(5)); // true
+            Console.WriteLine("Is 20 in the set? " + mySet.Contains(20)); // false
+
+            // Iterating through the set
+            Console.WriteLine("Set elements:");
+            foreach (int item in mySet)
             {
-                Console.WriteLine(num);
+                Console.WriteLine(item);
             }
         }
         static void Main(string[] args)
         {
-            new Program().UnderstandingList();
+            new Program().UnderstandingSet();
 
         }
     }
