7e991b349fdeadb448f6c9fd7c6b8b3a0221ac4b
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-17 10:34:27 +0530

    Introducing generics in practice

--- Day7/RequestTrackerApplication/RequestTrackerModelLibrary/Request.cs
+++ Day7/RequestTrackerApplication/RequestTrackerModelLibrary/Request.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RequestTrakerModelLibrary
+namespace RequestTrackerModelLibrary
 {
-    public class Request
+    public class Request : IEquatable<Request>
     {
         public int Id { get; set; }
         public string RequestText { get; set; }
         public int Raised_By { get; set; }
         public string Status { get; set; }
         public int Closed_By { get; set; }
+        public DateTime RaisedDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+
+        //public bool Equals(Request? other)
+        //{
+        //    return Id.Equals(other.Id);
+        //}
+        public bool Equals(Request? other) => Id.Equals(other.Id);
     }
 }
