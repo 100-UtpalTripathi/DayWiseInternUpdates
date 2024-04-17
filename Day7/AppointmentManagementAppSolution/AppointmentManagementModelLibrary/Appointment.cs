8f87320b815175f5e06984b1c45c5719f0730ac4
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-17 14:33:41 +0530

    Added Apppointed Library associating with Doctor and Patient!

--- Day7/AppointmentManagementAppSolution/AppointmentManagementModelLibrary/Appointment.cs
+++ Day7/AppointmentManagementAppSolution/AppointmentManagementModelLibrary/Appointment.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace AppointmentManagementModelLibrary
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public int PatientId { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+
+        public Doctor Doctor { get; set; }
+        public Patient Patient { get; set; }
+
+        public Appointment(int id, int doctorId, int patientId, DateTime appointmentDateTime)
+        {
+            Id = id;
+            DoctorId = doctorId;
+            PatientId = patientId;
+            AppointmentDateTime = appointmentDateTime;
+        }
     }
 }
