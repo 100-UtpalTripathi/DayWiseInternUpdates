b7bdb680a35bdd9924921abf67199d77a310b4ad
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-17 14:29:20 +0530

    Added Doctor Library

---
+++ Day7/AppointmentManagementAppSolution/AppointmentManagementModelLibrary/Doctor.cs
@@ -0,0 +1,21 @@
+﻿namespace AppointmentManagementModelLibrary
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+
+        public Doctor(int id, string name, string specialization, string contactNumber, string email)
+        {
+            Id = id;
+            Name = name;
+            Specialization = specialization;
+            ContactNumber = contactNumber;
+            Email = email;
+        }
+    }
+
+}
