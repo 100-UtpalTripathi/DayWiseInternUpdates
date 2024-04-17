dcb3cf969ab80c97f7f042fa09107574a7480610
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-17 10:42:39 +0530

    Practicing list in collections!

--- Day7/RequestTrackerApplication/RequestTrackerApplication/Program.cs
+++ Day7/RequestTrackerApplication/RequestTrackerApplication/Program.cs
@@ -1,23 +1,26 @@
 ﻿using RequestTrackerModelLibrary;
+using System.Collections;
 
 namespace RequestTrackerApp
 {
     internal class Program
     {
+        void UnderstaingList()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(100);
+            list.Add("Hiiii");
+            list.Add(23.4);
+            list.Add(90.3f);
+            list.Add(new Employee(101, "Stuart", new DateTime(), "HR"));
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
         static void Main(string[] args)
         {
-            Employee employee1, employee2;
-
-            employee1 = new Employee(101, "Ramu", new DateTime(2000, 12, 2), "Admin");
-            employee2 = new Employee(101, "Ramu", new DateTime(2000, 12, 2), "Admin");
-            if (employee1 == employee2)
-            {
-                Console.WriteLine("Both Same");
-            }
-            else
-            {
-                Console.WriteLine($"{employee1} and {employee2} are Not same employee");
-            }
+            new Program().UnderstaingList();
 
         }
     }
