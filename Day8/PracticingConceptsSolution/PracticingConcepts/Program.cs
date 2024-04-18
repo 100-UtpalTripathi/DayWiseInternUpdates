9444893835a7809e10da8050efb902a29010fe32
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-18 14:28:04 +0530

    Understanding Indexers!

--- Day8/PracticingConceptsSolution/PracticingConcepts/Program.cs
+++ Day8/PracticingConceptsSolution/PracticingConcepts/Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace PracticingConcepts
 {
-    // Program to implement the concept of method overloading
-    // i.e. functions having same name but different number and type of parameters...
+
+    internal class Student
+    {
+        public int Id { get; set; }
+        string[] skills = new string[3];
+        public string Name { get; set; }
+        public string this[int index]
+        {
+            get { return skills[index]; }
+            set { skills[index] = value; }
+        }
+        public override string ToString()
+        {
+            string data = Id + " " + Name + " ";
+            for (int i = 0; i < skills.Length; i++)
+                data += skills[i] + " ";
+            return data;
+        }
+    }
     internal class Program
     {
         // Method to add two integers
@@ -23,17 +40,23 @@
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("  $$  Implementing the Concept of Function Overloading! $$ \n");
-            Program p = new Program();
+            //Console.WriteLine("  $$  Implementing the Concept of Function Overloading! $$ \n");
+            //Program p = new Program();
 
-            // Call the overloaded methods
-            int sum1 = p.Add(2, 3);
-            int sum2 = p.Add(2, 3, 4);
-            double sum3 = p.Add(2.5, 3.5);
+            //// Call the overloaded methods
+            //int sum1 = p.Add(2, 3);
+            //int sum2 = p.Add(2, 3, 4);
+            //double sum3 = p.Add(2.5, 3.5);
 
-            Console.WriteLine("Sum of 2 and 3: " + sum1);
-            Console.WriteLine("Sum of 2, 3, and 4: " + sum2);
-            Console.WriteLine("Sum of 2.5 and 3.5: " + sum3);
+            //Console.WriteLine("Sum of 2 and 3: " + sum1);
+            //Console.WriteLine("Sum of 2, 3, and 4: " + sum2);
+            //Console.WriteLine("Sum of 2.5 and 3.5: " + sum3);
+
+            Student student = new Student() { Name = "Ramu", Id = 101 };
+            student[0] = "C";
+            student[1] = "Java";
+            student[2] = "C#";
+            Console.WriteLine(student);
         }
     }
 }
