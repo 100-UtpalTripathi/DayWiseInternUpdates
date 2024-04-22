ad222b638c8ea4140119eee9f49edb4760b1604d
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-22 10:27:24 +0530

    Combined and created generic Interface for User and Quiz!

---
+++ Day9/QuizApplicationSolution/QuizApplicationDALLibrary/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizApplicationModelLibrary;
+
+namespace QuizApplicationDALLibrary
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        T Get(string id);
+        List<T> GetAll();
+        void Update(string id, T updatedEntity);
+        void Delete(string id);
+    }
+}
