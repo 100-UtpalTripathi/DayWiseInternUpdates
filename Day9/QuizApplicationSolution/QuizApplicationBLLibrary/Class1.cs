ee446065bdbeebe7893fc56c701049b91020113d
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-22 11:02:41 +0530

    Defined Interface for Quiz Related Service methods!

---
+++ Day9/QuizApplicationSolution/QuizApplicationBLLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace QuizApplicationBLLibrary
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Day9/QuizApplicationSolution/QuizApplicationBLLibrary/IQuizService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizApplicationModelLibrary;
+namespace QuizApplicationBLLibrary
+{
+    public interface IQuizService
+    {
+        Quiz CreateQuiz(string title, string description, List<Question> questions);
+        void EditQuiz(string title, Quiz updatedQuiz);
+        void DeleteQuiz(string title);
+        List<Quiz> GetPublishedQuizzes();
+        Quiz GetQuiz(string title);
+        void PublishQuiz(string title);
+    }
+
+    public interface IQuizTakingService
+    {
+        void TakeQuiz(Quiz quiz);
+        void ReviewLastQuizAttempt();
+    }
+}
