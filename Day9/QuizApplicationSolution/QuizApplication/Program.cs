503fde1f41af2e41819ee80d7bbe89d7787cf9b5
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-22 10:03:57 +0530

    Project setup completed, Quiz model created!

---
+++ Day9/QuizApplicationSolution/QuizApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Day9/QuizApplicationSolution/QuizApplicationModelLibrary/Quiz.cs
@@ -0,0 +1,19 @@
+﻿
+namespace QuizApplicationModelLibrary
+{
+
+    // Quiz class represents a quiz with a title, description, and a list of questions.
+    public class Quiz
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Quiz(string title, string description, List<Question> questions)
+        {
+            Title = title;
+            Description = description;
+            Questions = questions;
+        }
+    }
+}
