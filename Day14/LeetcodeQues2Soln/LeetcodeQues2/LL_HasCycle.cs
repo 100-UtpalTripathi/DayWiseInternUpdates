d1a0144a3d0d5887a6325dcb7056acc5e44c17d4
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-29 15:14:13 +0530

    Leetcode Question(LL has cycle?) Solution added!

---
+++ Day14/LeetcodeQues2Soln/LeetcodeQues2/LL_HasCycle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LeetcodeQues2
+{
+    internal class LL_HasCycle
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) 
+                return false;
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static void Main(string[] args)
+        { 
+            ListNode head = new ListNode(1);
+            ListNode node2 = new ListNode(2);
+            ListNode node3 = new ListNode(3);
+            ListNode node4 = new ListNode(4);
+            ListNode node5 = new ListNode(5);
+
+            head.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+            node5.next = node2; // creates a cycle
+
+            LL_HasCycle solution = new LL_HasCycle();
+            bool hasCycle = solution.HasCycle(head);
+            Console.WriteLine("Linked list has cycle: " + hasCycle);
+        }
+    }
+}
