3cc972823431d9ddfbc42f48850472be1b614a5e
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-04-29 14:38:23 +0530

    Added Solution of leetcode question(Excel Sheet Column Title)

---
+++ Day14/LeetcodeQuesSoln/LeetcodeQues/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Excel_Sheet_Column_Title
+{
+    internal class Program
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            string result = "";
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                result = (char)('A' + columnNumber % 26) + result;
+                columnNumber /= 26;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of test cases: ");
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            Program p = new Program();
+            while (t-- > 0)
+            {
+                Console.WriteLine("Enter the column number: ");
+                int columnNumber = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("The column title is: " + p.ConvertToTitle(columnNumber));
+            }
+        }
+    }
+}
