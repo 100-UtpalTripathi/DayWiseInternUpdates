018edd820cfcd339e442408b6adc348370809f0a
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-05-12 15:55:44 +0530

    Added Business Logic Interfaces!

---
+++ Day22/RequestTrackerModelLibrarySolution/RequestTrackerBLLibrary/IRequestService.cs
@@ -0,0 +1,17 @@
+﻿using RequestTrackerModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerBLLibrary
+{
+    public interface IRequestService   // handles operations related to requests for both users and admins.
+    {
+        Task<Request> RaiseRequest(Request request);
+        Task<ICollection<Request>> ViewRequestStatus(Employee employee);
+        Task<ICollection<RequestSolution>> ViewSolutions(int requestId);
+        Task<bool> RespondToSolution(int requestId, string response);
+    }
+}
