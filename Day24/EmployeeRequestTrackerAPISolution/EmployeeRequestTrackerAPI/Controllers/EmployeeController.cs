3304def70749a3979e633beca69979a0c98fc928
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-05-14 11:21:26 +0530

    Created first API and learned Dependency Injection concept!

---
+++ Day24/EmployeeRequestTrackerAPISolution/EmployeeRequestTrackerAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeRequestTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Day24/PracticeWebApiSoln/PracticeWebApi/Controllers/DemoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Hello World {id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok($"Hello World {value}");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok($"Hello World {id} {value}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Hello World {id}");
+        }
+    }
+}
